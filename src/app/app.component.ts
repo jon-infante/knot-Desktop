abde175415e466a4efff194d8a96832811574e24
Jon Infante <jonjoninfante@gmail.com>
2022-12-08 13:19:12 -0500

    colorful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { KnotComponent } from 'src/root/components/screens/home/subcomponents/knot/knot.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'site';
+  
 }
